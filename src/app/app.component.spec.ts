929afe31a6f667b946418445a133407c147a69f8
doobie <david_bradyw@yahoo.com>
2021-07-31 11:20:36 -0400

    renamed old app name to frontend

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'serverless-stripe'`, () => {
+  it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('serverless-stripe');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain(
-      'serverless-stripe app is running!'
+      'frontend app is running!'
     );
   });
 });
